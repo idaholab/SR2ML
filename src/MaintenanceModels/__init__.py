f2de6d8b11f92da10a2f8c219c37dfe668a00d6f
mandd <diego.mandelli@inl.gov>
2020-05-15 13:41:15 -0600

    edits

--- src/MaintenanceModels/__init__.py
+++ src/MaintenanceModels/__init__.py
@@ -10,6 +10,8 @@ Created on April 23rd, 2020
 from __future__ import absolute_import
 
 from .PMmodel import PMModel
-
+from .Factory import knownTypes
+from .Factory import returnInstance
+from .Factory import returnClass
 
 __all__ = ['PMModel']
