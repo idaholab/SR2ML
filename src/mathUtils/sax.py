0afe4ef295decf6106e6f227a35a8f295534753d
mandd <diego.mandelli@inl.gov>
2021-03-30 12:06:22 -0600

    restructuring of sax class

--- src/mathUtils/sax.py
+++ src/mathUtils/sax.py
@@ -12,119 +12,131 @@ from scipy.stats import norm
 import string
 # Internal Imports
 
-def piecewiseAggregateApproximation(data,w):
+class SAX():
   """
-    This method performs Piecewise Aggregate Approximation of the given time series
-    @ In, data, pandas DataFrame, time series to be discretized
-    @ In, w, int, number of discretization points
-    @ Out, paa, pandas DataFrame, discretized time series
-
-    Reference: J. Lin, E. Keogh, L. Wei, and S. Lonardi 
-               Experiencing SAX: a Novel Symbolic Representation of Time Series. 
-               Data Mining and Knowledge Discovery Journal (2007).
+  Class containing the algorithm which performs symbolic conversion of time series using the SAX algorithm
+  
+  Reference: Lin, J., Keogh, E., Wei, L. and Lonardi, S. (2007). 
+             Experiencing SAX: a Novel Symbolic Representation of Time Series. 
+             Data Mining and Knowledge Discovery Journal.
+  
+  Link: https://www.cs.ucr.edu/~eamonn/SAX.htm
+  """
+  
+  def __init__(self, timeWindows, alphabetSizeDict):
+    """
+      This method initializes the SAX class
+      @ In, alphabetSizeDict, dict, discretization size for each dimensions
+      @ In, timeWindows, int, discretization of the time axis
+      @ Out, symbolicTS, pandas DataFrame, symbolic conversion of provided time series
+    """
+    self.timeWindows = timeWindows
+    self.alphabetSizeDict = alphabetSizeDict
+  
+  def fit(self, data, normalization=True):
+    """
+      This method performs symbolic conversion of time series using the SAX algorithm
+      @ In, data, pandas DataFrame, 
+      @ In, normalization, bool, parameter that set if time series normalization is required (True) or not (False)
+      @ Out, symbolicTS, pandas DataFrame, symbolic conversion of provided time series
+      @ Out, varCutPoints, dict, dictionary containing the discretization points for each dimension
+      @ Out, symbolicTS, normalizationData, dictionary containing the parameters used to performe normalization of each dimension
+    """
+    # Normalize data
+    if normalization:  
+      normalizedData, normalizationData = self.timeSeriesNormalization(data)
     
-    Link: https://www.cs.ucr.edu/~eamonn/SAX.htm
-  """
-  nTimeVals, nVars = data.shape
-  
-  newDate = pd.date_range(data.index.to_numpy()[0], data.index.to_numpy()[-1], periods=w)
-  
-  paaData = {}
-  for var in data:
-    res = np.zeros(w)
-    if (nTimeVals % w == 0):
-      inc = nTimeVals // w
-      for i in range(0, nTimeVals):
-        idx = i // inc
-        res[idx] = res[idx] + data[var][i]
-      paaData[var] = res / inc
-    else:
-      for i in range(0, w * nTimeVals):
-        idx = i // nTimeVals
-        pos = i // w
-        res[idx] = res[idx] + data[var][pos]
-      paaData[var] = res / nTimeVals
-  
-  paa = pd.DataFrame(paaData, index=newDate)
-
-  return paa
-
-
-def timeSeriesNormalization(data):
-  """
-    This method performs the Z-normalization of a given time series
-    @ In, data, pandas DataFrame, time series to be normalized
-    @ Out, data, pandas DataFrame, normalized time series
-    @ Out, normalizationData, dict, dictionary containing mean and std-dev of each dimension of the time series
-  """
-  normalizationData = {}
-  for var in data:
-    normalizationData[var] = [np.mean(data[var].values),np.std(data[var].values)]
-    data[var] = (data[var]-normalizationData[var][0])/normalizationData[var][1]  
-  return data, normalizationData
-
-
-def ndTS2String(paaTimeSeries, alphabetSizeDict):  
-  """
-    This method performs the symbolic conversion of a given time series
-    @ In, data, pandas DataFrame, multi-variate time series to be converted into string
-    @ Out, paaTimeSeries, pandas DataFrame, symbolic converted time series
-    @ Out, varCutPoints, dict, dictionary containing cuts data for each dimension
-  """
-  varCutPoints = {}
-  
-  for var in paaTimeSeries:
-    varCutPoints[var] = norm.ppf(np.linspace(0.0, 1.0, num=alphabetSizeDict[var]+1),loc=0., scale=1.)
-    paaTimeSeries[var] = ts2String(paaTimeSeries[var], varCutPoints[var])
-  
-  return paaTimeSeries, varCutPoints
-      
-def ts2String(series, cuts): 
-  """
-    This method performs the symbolic conversion of a single time series
-    @ In, data, pandas DataFrame, uni-variate time series to be converted into string
-    @ In, cuts, dict, dictionary containing cuts data for the considered time series
-    @ Out, charArray, np.array, symbolic converted time series
-  """
-  alphabetString = string.ascii_uppercase
-  alphabetList = list(alphabetString) 
-
-  series = np.array(series)
-  charArray = np.chararray(series.shape[0],unicode=True) 
-  
-  for i in range(series.shape[0]):
-    j=0
-    while cuts[j]<series[i]:
-      j=j+1
-    charArray[i] = alphabetList[j-1]
-
-  return charArray
-
-
-def SAXtimePoints(data, alphabetSizeDict, symbolicSeriesLength, normalization=True):
-  """
-    This method perform symbolic conversion of time series using the SAX algorithm
-    @ In, data, pandas DataFrame, 
-    @ In, alphabetSize, dict, discretization size for each dimensions
-    @ In, timeSeriesLength, int, discretization of the time axis
-    @ Out, symbolicTS, pandas DataFrame, symbolic conversion of provided time series
-
-    Reference: Lin, J., Keogh, E., Wei, L. and Lonardi, S. (2007). 
-               Experiencing SAX: a Novel Symbolic Representation of Time Series. 
-               Data Mining and Knowledge Discovery Journal.
+    # PAA process
+    paaData = self.piecewiseAggregateApproximation(normalizedData)
     
-    Link: https://www.cs.ucr.edu/~eamonn/SAX.htm
-  """
-  # Normalize data
-  if normalization:  
-    normalizedData, normalizationData = timeSeriesNormalization(data)
+    symbolicData,varCutPoints = self.ndTS2String(paaData)
+    
+    return symbolicData, varCutPoints, normalizationData
+    
+
+  def piecewiseAggregateApproximation(self, data):
+    """
+      This method performs Piecewise Aggregate Approximation of the given time series
+      @ In, data, pandas DataFrame, time series to be discretized
+      @ Out, paa, pandas DataFrame, discretized time series
+    """
+    nTimeVals, nVars = data.shape
+    
+    newDate = pd.date_range(data.index.to_numpy()[0], data.index.to_numpy()[-1], periods=self.timeWindows)
+    
+    paaData = {}
+    for var in data:
+      res = np.zeros(self.timeWindows)
+      if (nTimeVals % self.timeWindows == 0):
+        inc = nTimeVals // self.timeWindows
+        for i in range(0, nTimeVals):
+          idx = i // inc
+          res[idx] = res[idx] + data[var][i]
+        paaData[var] = res / inc
+      else:
+        for i in range(0, self.timeWindows * nTimeVals):
+          idx = i // nTimeVals
+          pos = i // self.timeWindows
+          res[idx] = res[idx] + data[var][pos]
+        paaData[var] = res / nTimeVals
+    
+    paa = pd.DataFrame(paaData, index=newDate)
   
-  # PAA process
-  paaData = piecewiseAggregateApproximation(normalizedData,symbolicSeriesLength)
+    return paa
   
-  symbolicData,varCutPoints = ndTS2String(paaData, alphabetSizeDict)
   
-  return symbolicData,varCutPoints,normalizationData
+  def timeSeriesNormalization(self, data):
+    """
+      This method performs the Z-normalization of a given time series
+      @ In, data, pandas DataFrame, time series to be normalized
+      @ Out, data, pandas DataFrame, normalized time series
+      @ Out, normalizationData, dict, dictionary containing mean and std-dev of each dimension of the time series
+    """
+    normalizationData = {}
+    
+    for var in data:
+      normalizationData[var] = [np.mean(data[var].values),np.std(data[var].values)]
+      data[var] = (data[var]-normalizationData[var][0])/normalizationData[var][1]  
+    
+    return data, normalizationData
+  
+  
+  def ndTS2String(self, paaTimeSeries):  
+    """
+      This method performs the symbolic conversion of a given time series
+      @ In, data, pandas DataFrame, multi-variate time series to be converted into string
+      @ Out, paaTimeSeries, pandas DataFrame, symbolic converted time series
+      @ Out, varCutPoints, dict, dictionary containing cuts data for each dimension
+    """
+    varCutPoints = {}
+    
+    for var in paaTimeSeries:
+      varCutPoints[var] = norm.ppf(np.linspace(0.0, 1.0, num=self.alphabetSizeDict[var]+1),loc=0., scale=1.)
+      paaTimeSeries[var] = self.ts2String(paaTimeSeries[var], varCutPoints[var])
+    
+    return paaTimeSeries, varCutPoints
+        
+  def ts2String(self, series, cuts): 
+    """
+      This method performs the symbolic conversion of a single time series
+      @ In, series, pandas DataFrame, uni-variate time series to be converted into string
+      @ In, cuts, dict, dictionary containing cuts data for the considered time series
+      @ Out, charArray, np.array, symbolic converted time series
+    """
+    alphabetString = string.ascii_uppercase
+    alphabetList = list(alphabetString) 
+  
+    series = np.array(series)
+    charArray = np.chararray(series.shape[0],unicode=True) 
+    
+    for i in range(series.shape[0]):
+      j=0
+      while cuts[j]<series[i]:
+        j=j+1
+      charArray[i] = alphabetList[j-1]
+  
+    return charArray
+
 
 
 ''' testing '''
@@ -141,7 +153,8 @@ df.plot()
 
 alphabetSizeDict={}
 alphabetSizeDict['var1']=10
-sax,cuts,normData = SAXtimePoints(df, alphabetSizeDict, 20, normalization=True)
+saxConverter = SAX(20,alphabetSizeDict)
+sax,cuts,normData = saxConverter.fit(df, normalization=True)
 print(sax)
 
 for val in cuts['var1']:
