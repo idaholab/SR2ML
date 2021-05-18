876f7e08452c40e96f01b35163bbc933dc1212fd
mandd <diego.mandelli@inl.gov>
2021-05-18 16:38:16 -0600

    main files

---
+++ src/MarginModels/PointSetMarginModel.py
@@ -0,0 +1,106 @@
+# Copyright 2020, Battelle Energy Alliance, LLC
+# ALL RIGHTS RESERVED
+"""
+Created on May 13 2021
+
+@author: mandd,wangc
+"""
+
+#External Modules------------------------------------------------------------------------------------
+import abc
+import sys
+import os
+import numpy as np
+import pandas as pd
+#External Modules End--------------------------------------------------------------------------------
+
+#Internal Modules------------------------------------------------------------------------------------
+from utils import mathUtils as utils
+from utils import InputData, InputTypes
+from .MarginBase import MarginBase
+from scipy.stats import wasserstein_distance
+from sklearn.metrics import pairwise_distances
+#Internal Modules End--------------------------------------------------------------------------------
+
+
+class PointSetMarginModel(MarginBase):
+  
+  @classmethod
+  def getInputSpecification(cls):
+    """
+      Collects input specifications for this class.
+      @ In, cls, class instance
+      @ Out, inputSpecs, InputData, specs
+    """ 
+    inputSpecs = super(PointSetMarginModel, cls).getInputSpecification()
+    inputSpecs.description = """ PointSet Margin Model """ 
+    inputSpecs.addSub(InputData.parameterInputFactory('failedDataFile',    contentType=InputTypes.InterpretedListType, descr='failed data file'))
+
+
+  def __init__(self):
+    """
+      Constructor
+      @ In, None
+      @ Out, None
+    """
+    MarginBase.__init__(self)  
+    
+    self.failedDataFileID = None 
+    self.columnID = None 
+    self.actualDataID = None 
+    
+
+  def _localHandleInput(self, paramInput):
+    """
+      Function to read the portion of the parsed xml input that belongs to this specialized class
+      and initialize some stuff based on the inputs got
+      @ In, paramInput, InputData.ParameterInput, the parsed xml input
+      @ Out, None
+    """
+    MarginBase._localHandleInput(self, paramInput)
+    
+    for child in paramInput.subparts:
+      if child.getName().lower() == 'failedDataFileID':
+        self.failedDataFileID = child.value 
+      if child.getName().lower() == 'columnID':
+        self.columnID = child.value   
+      if child.getName().lower() == 'actualDataID':
+        self.actualDataID = child.value 
+    
+    self.failedData = pd.read_csv(self.failedDataFileID)[self.columnID]
+
+  def initialize(self, inputDict):
+    """
+      Method to initialize this class
+      @ In, inputDict, dict, dictionary of inputs
+      @ Out, None
+    """
+    MarginBase.initialize(self, inputDict)    
+    
+    
+  def _marginFunction(self, inputDict):
+    """
+      Method to calculate margin value
+      @ In, inputDict, dict, dictionary of inputs
+      @ Out, margin, float, value of margin for the considered model
+    """
+    actualData = inputDict[self.actualDataID] 
+    
+    zeroPoint = np.zeros(1)
+    
+    distMatrix = pairwise_distances(self.failedData.reshape(-1,1), actualData.reshape(-1,1), metric=customDist)
+    distMatrix[distMatrix<0] = 0
+    margin = np.mean(distMatrix)
+    
+    distMatrix2 = pairwise_distances(self.failedData.reshape(-1,1), zeroPoint.reshape(-1,1), metric=customDist)
+    distMatrix2[distMatrix2<0] = 0
+    margin2 = np.mean(distMatrix2)
+    
+    normalizedMargin = margin/margin2
+    
+    return normalizedMargin 
+  
+def customDist(a,b):
+  return a-b
+    
+    
---
+++ src/MarginModels/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2020, Battelle Energy Alliance, LLC
+# ALL RIGHTS RESERVED
+"""
+Created on May 18th, 2021
+@author: mandd, wangc
+"""
+
+from __future__ import absolute_import
+
+from .PointSetMarginModel import PointSetMarginModel
+from .Factory import knownTypes
+from .Factory import returnInstance
+from .Factory import returnClass
+
+__all__ = ['PointSetMarginModel']
