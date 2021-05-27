c741d823fd27741d71ccfb978eb722ebaef2d6e1
mandd <diego.mandelli@inl.gov>
2021-05-26 18:30:50 -0600

    edits

--- src/MarginModels/PointSetMarginModel.py
+++ src/MarginModels/PointSetMarginModel.py
@@ -24,16 +24,16 @@ from sklearn.metrics import pairwise_distances
 
 
 class PointSetMarginModel(MarginBase):
-  
+
   @classmethod
   def getInputSpecification(cls):
     """
       Collects input specifications for this class.
       @ In, cls, class instance
       @ Out, inputSpecs, InputData, specs
-    """ 
+    """
     inputSpecs = super(PointSetMarginModel, cls).getInputSpecification()
-    inputSpecs.description = """ PointSet Margin Model """ 
+    inputSpecs.description = """ PointSet Margin Model """
     inputSpecs.addSub(InputData.parameterInputFactory('failedDataFile',    contentType=InputTypes.InterpretedListType, descr='failed data file'))
 
 
@@ -43,12 +43,12 @@ class PointSetMarginModel(MarginBase):
       @ In, None
       @ Out, None
     """
-    MarginBase.__init__(self)  
-    
-    self.failedDataFileID = None 
-    self.columnID = None 
-    self.actualDataID = None 
-    
+    MarginBase.__init__(self)
+
+    self.failedDataFileID = None
+    self.columnID = None
+    self.actualDataID = None
+
 
   def _localHandleInput(self, paramInput):
     """
@@ -58,15 +58,15 @@ class PointSetMarginModel(MarginBase):
       @ Out, None
     """
     MarginBase._localHandleInput(self, paramInput)
-    
+
     for child in paramInput.subparts:
       if child.getName().lower() == 'failedDataFileID':
-        self.failedDataFileID = child.value 
+        self.failedDataFileID = child.value
       if child.getName().lower() == 'columnID':
-        self.columnID = child.value   
+        self.columnID = child.value
       if child.getName().lower() == 'actualDataID':
-        self.actualDataID = child.value 
-    
+        self.actualDataID = child.value
+
     self.failedData = pd.read_csv(self.failedDataFileID)[self.columnID]
 
   def initialize(self, inputDict):
@@ -75,32 +75,32 @@ class PointSetMarginModel(MarginBase):
       @ In, inputDict, dict, dictionary of inputs
       @ Out, None
     """
-    MarginBase.initialize(self, inputDict)    
-    
-    
+    MarginBase.initialize(self, inputDict)
+
+
   def _marginFunction(self, inputDict):
     """
       Method to calculate margin value
       @ In, inputDict, dict, dictionary of inputs
       @ Out, margin, float, value of margin for the considered model
     """
-    actualData = inputDict[self.actualDataID] 
-    
+    actualData = inputDict[self.actualDataID]
+
     zeroPoint = np.zeros(1)
-    
+
     distMatrix = pairwise_distances(self.failedData.reshape(-1,1), actualData.reshape(-1,1), metric=customDist)
     distMatrix[distMatrix<0] = 0
     margin = np.mean(distMatrix)
-    
+
     distMatrix2 = pairwise_distances(self.failedData.reshape(-1,1), zeroPoint.reshape(-1,1), metric=customDist)
     distMatrix2[distMatrix2<0] = 0
     margin2 = np.mean(distMatrix2)
-    
+
     normalizedMargin = margin/margin2
-    
-    return normalizedMargin 
-  
+
+    return normalizedMargin
+
 def customDist(a,b):
   return a-b
-    
-    
+
+
