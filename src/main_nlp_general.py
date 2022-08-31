42e026db7fd6f7ee3cf253cabbe6cf4ae70dc589
Congjian Wang <congjian.wang@inl.gov>
2022-08-31 15:05:03 -0600

    add new pipelines for simple match and phrase match

--- src/main_nlp_general.py
+++ src/main_nlp_general.py
@@ -10,6 +10,9 @@ import logging
 from nlp.RuleBasedMatcher import RuleBasedMatcher
 import spacy
 
+from nlp.SimpleEntityMatcher import SimpleEntityMatcher
+from nlp.PhraseEntityMatcher import PhraseEntityMatcher
+
 logging.basicConfig(format='%(asctime)s %(name)-20s %(levelname)-8s %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
 # To enable the logging to both file and console, the logger for the main should be the root,
 # otherwise, a function to add the file handler and stream handler need to be created and called by each module.
@@ -25,6 +28,10 @@ logger.addHandler(fh)
 
 if __name__ == "__main__":
   nlp = spacy.load("en_core_web_lg")
+  # remove the 'ner' pipeline which is used for ORG and DATE entity recognization
+  if nlp.has_pipe('ner'):
+    nlp.remove_pipe('ner')
+
   doc = r"""A leak was noticed from the RCP pump 1A.
 RCP pump 1A pressure gauge was found not operating.
 RCP pump 1A pressure gauge was found inoperative.
@@ -51,53 +58,23 @@ Pump bearings appear in acceptable condition.
 Pump made noises - not nough to affect performance.
 Pump shaft has a slight deflection.
         """
-  matcher = RuleBasedMatcher(nlp, match=True, phraseMatch=True)
 
-  ##Issue with simple and phrase matcher, if there are duplicate names, callback functions
-  ##can not be used, in which case, we can not directly extend doc.ents, which will raise the
-  ## error:  Unable to set entity information for token xx which is included in more than
-  ## one span in entities, blocked, missing or outside. (https://github.com/explosion/spaCy/discussions/9993)
-  ## In this case, we need to use "entity_ruler" to identify the matches
+
   # simple match
-  # name = 'ssc_match'
-  # rules = [{"LOWER":"pump"}, {"POS":"NOUN"}]
-  # matcher.addPattern(name, rules, callback=None)
+  slabel = 'ssc_match'
+  terms = [{"LOWER":"pump"}, {"POS":"NOUN"}]
+  logger.debug('Add pattern for simple entity matcher')
+  nlp.add_pipe('simple_entity_matcher', config={"label": slabel, "terms":terms, "asSpan":True})
   # phrase match
-  name = 'ssc_phrase_match'
-  # phraseList = ["safety cage", 'pump 1A', 'pump bearings', 'Pump']
-  phraseList = ["safety cage", "cage"]
-  matcher.addPhrase(name, phraseList, callback=None)
-  # depency match
-  name = 'ssc_dependency_match'
-  dependencyList = [
-    {
-      "RIGHT_ID":"anchor_found",
-      "RIGHT_ATTRS":{"ORTH":"found"}
-    },
-    {
-      "LEFT_ID":"anchor_found",
-      "REL_OP":">",
-      "RIGHT_ID":"subject",
-      "RIGHT_ATTRS":{"DEP":"nsubjpass"}
-    },
-    {
-      "LEFT_ID":"anchor_found",
-      "REL_OP":">",
-      "RIGHT_ID":"object",
-      "RIGHT_ATTRS":{"DEP":"oprd"}
-    }
-  ]
-  # matcher.addDependency(name, dependencyList, callback=None)
-  # Entity rule-based match
-  name = 'ssc_entity_ruler'
-  patterns = [{"label":"pump_comp", "pattern":[{"POS":"NOUN", "OP":"*"}, {"LOWER":"pump"}, {"POS":"NOUN", "OP":"*"}], "id":"SSC"},
-              {"label":"pump_comp", "pattern":[{"POS":"NOUN", "OP":"*"}, {"LOWER":"shaft"}, {"POS":"NOUN", "OP":"*"}], "id":"SSC"},
-              {"label":"pump_comp", "pattern":[{"POS":"NOUN", "OP":"*"}, {"LOWER":"gauge"}, {"POS":"NOUN", "OP":"*"}], "id":"SSC"},
-              {"label":"pump_comp", "pattern":[{"POS":"NOUN", "OP":"*"}, {"LOWER":"bearing"}, {"POS":"NOUN", "OP":"*"}], "id":"SSC"},
-              {"label":"pump_comp", "pattern":[{"POS":"NOUN", "OP":"*"}, {"LOWER":"supply"}, {"POS":"NOUN", "OP":"*"}], "id":"SSC"},
-              {"label":"pump_comp", "pattern":[{"POS":"NOUN", "OP":"*"}, {"LOWER":"impeller"}, {"POS":"NOUN", "OP":"*"}], "id":"SSC"},
-              {"label":"pump_comp", "pattern":[{"POS":"NOUN", "OP":"*"}, {"LOWER":"flow meter"}, {"POS":"NOUN", "OP":"*"}], "id":"SSC"},
-              {"label":"pump_comp", "pattern":[{"POS":"NOUN", "OP":"*"}, {"LOWER":"motor"}, {"POS":"NOUN", "OP":"*"}], "id":"SSC"}]
-  # matcher.addEntityPattern(name, patterns)
+  plabel = 'ssc_phrase_match'
+  terms = ["safety cage", "Oil puddle", "OPS"]
+  logger.debug('Add pattern for phrase entity matcher')
+  nlp.add_pipe('phrase_entity_matcher', config={"label": plabel, "terms":terms, "asSpan":True})
+  newDoc = nlp(doc)
 
-  matcher(doc)
+  sEnts = [ent for ent in newDoc.ents if ent.label_==slabel]
+  pEnts = [ent for ent in newDoc.ents if ent.label_==plabel]
+  logger.debug('Simple matches:')
+  logger.debug(sEnts)
+  logger.debug('Phrase matches:')
+  logger.debug(pEnts)
