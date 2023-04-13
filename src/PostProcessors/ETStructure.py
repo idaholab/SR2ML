aff82692bc175828c668874b8ba8f13a0745926c
Congjian Wang <congjian.wang@inl.gov>
2023-04-13 09:05:08 -0600

    Revert "Merge remote-tracking branch 'origin/devel' into mandd/tag_keywords_lists"
    This reverts commit c4165afb040968b2c2ec4d7a3c195677b7d6b9c1, reversing
    changes made to f0282675f258af564e279fbc4650685a037245d2.

--- src/PostProcessors/ETStructure.py
+++ src/PostProcessors/ETStructure.py
@@ -303,7 +303,7 @@ class ETStructure(object):
     seqID        = []
 
     for node in root.findall('define-sequence'):
-      for child in node.iter():
+      for child in node.getiterator():
         if 'event-tree' in child.tag:
           dependencies.append(child.get('name'))
           seqID.append(node.get('name'))
@@ -458,7 +458,7 @@ class ETStructure(object):
         ## fill in the details under this branch, later iterations will
         ## correct lower rows if a path does change
         X[rowCounter, col] = val
-        for fork in iter(path):
+        for fork in path.getchildren():
           newCounter = self.constructPointDFS(fork, inputMap, stateMap, outputMap, X, rowCounter)
           for i in range(newCounter-rowCounter):
             X[rowCounter+i, col] = val
